bae448cf70ee13c4b7dac52e3704bbbd136b4211
step <ammon@meso.net>
2015-10-19 00:14:35 +0200

    cleanup

--- Werkschau/Hansl_Bastian/inc.connectDB.php
+++
@@ -1,6 +0,0 @@
-<?
-$conn = new mysqli(SERVER, USER, PASSWORD, DATABASE);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-?>
--- Werkschau/Hansl_Bastian/inc.constants.php
+++
@@ -1,6 +0,0 @@
-<?
-define("USER", "db438138");
-define("PASSWORD", "RkOSwFvq");
-define("SERVER", "mysql5.ba573.de");
-define("DATABASE", "db438138");
-?>
